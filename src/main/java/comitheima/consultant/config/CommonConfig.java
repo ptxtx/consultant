8520bead68c46f2934584904b6ba1bf65a977484
ptx <3351626369@qq.com>
2026-06-12 15:20:02 +0800

    核心API

--- src/main/java/comitheima/consultant/config/CommonConfig.java
+++ src/main/java/comitheima/consultant/config/CommonConfig.java
@@ -1,9 +1,13 @@
 package comitheima.consultant.config;
 
 import comitheima.consultant.aiservice.ConsultantService;
+import dev.langchain4j.community.store.embedding.redis.RedisEmbeddingStore;
 import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.DocumentLoader;
+import dev.langchain4j.data.document.DocumentSplitter;
 import dev.langchain4j.data.document.loader.ClassPathDocumentLoader;
+import dev.langchain4j.data.document.parser.apache.pdfbox.ApachePdfBoxDocumentParser;
+import dev.langchain4j.data.document.splitter.DocumentSplitters;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
@@ -27,6 +31,8 @@ public class CommonConfig {
     private OpenAiChatModel openAiChatModel;
     @Autowired
     private ChatMemoryStore redisChatMemoryStore;
+    @Autowired
+    private RedisEmbeddingStore redisEmbeddingStore;
 //    @Bean
 //    public ConsultantService consultantService(){
 //        ConsultantService consultantService = AiServices.builder(ConsultantService.class)//要创建哪个接口，就传入哪个
@@ -63,24 +69,31 @@ public class CommonConfig {
 @Bean
     //构建向量数据库操作对象
     public EmbeddingStore store() {
-        //1.加载文档
-        List<Document> documents = ClassPathDocumentLoader.loadDocuments("content");
-        //2.构建向量数据库操作对象
-        InMemoryEmbeddingStore store = new InMemoryEmbeddingStore<>();
-        //3.构建一个EmbeddingStoreIngestor对象，完成文本数据切割，向量化，存储
-        EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
-                .embeddingStore(store)
-                .build();
-        ingestor.ingest(documents);
+    //1.加载文档
+    //List<Document> documents = ClassPathDocumentLoader.loadDocuments("content");
+    List<Document> documents = ClassPathDocumentLoader.loadDocuments("content", new ApachePdfBoxDocumentParser());
+    //2.构建向量数据库操作对象(操作的是内存版本的向量数据库）
+   // InMemoryEmbeddingStore store = new InMemoryEmbeddingStore<>();
+    //构建文档分割器对象
+    DocumentSplitter ds = DocumentSplitters.recursive(500, 100);
+    //3.构建一个EmbeddingStoreIngestor对象，完成文本数据切割，向量化，存储
+    EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
+           // .embeddingStore(store)
+            .embeddingStore(redisEmbeddingStore)
+            .documentSplitter(ds)
+            .build();
+    ingestor.ingest(documents);
 
-        return store;
-    }
+    //return store;
+    return redisEmbeddingStore;
+}
 
     @Bean
     //构建向量数据库检索对象
-    public ContentRetriever contentRetriever(EmbeddingStore store){
+    public ContentRetriever contentRetriever(/*EmbeddingStore store*/){
         return EmbeddingStoreContentRetriever.builder()
-                .embeddingStore(store)
+                //.embeddingStore(store)
+                .embeddingStore(redisEmbeddingStore)
                 .maxResults(3)
                 .minScore(0.5)
                 .build();
